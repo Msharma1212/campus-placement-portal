edfd28ff5c0ce481630ef2431470f1c0a877a115
Msharma1212 <kmayank122004@gmail.com>
2026-07-25 23:20:16 +0530

    Initialize profile editor component

---
+++ src/components/profile/ProfileEditor.tsx
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React, { useState } from 'react';
+import { useApp } from '../../context/AppContext';
+import { User } from '../../types';
+import { User as UserIcon, Plus, X, Save, UserCircle } from 'lucide-react';
+import { motion } from 'motion/react';
